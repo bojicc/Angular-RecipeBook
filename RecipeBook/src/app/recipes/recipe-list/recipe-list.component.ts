9f7361392fafe571e754d9d216fff87d0a535af5
Dejan <dejan.bojic.001@gmail.com>
2024-05-10 16:30:49 +0200

    Added Recipe and Ingredient model, changed .html and .ts components

--- RecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model'; 
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.foodandwine.com/thmb/fVmYbaQzXCz1Prx8VxrW9sMcjMU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Braciole-FT-RECIPE1122-66acf49cef0e4390bec780945709e7f3.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.foodandwine.com/thmb/fVmYbaQzXCz1Prx8VxrW9sMcjMU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Braciole-FT-RECIPE1122-66acf49cef0e4390bec780945709e7f3.jpg')
+  ];
 }
